90f6e9da862ceb76bcebb7908059a0c38295ea69
asadhossain <developerasad007@gmail.com>
2023-03-02 01:02:13 +0600

    a

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Contact Form</title>
+</head>
+<body>
+
+    <form action="" method= "POST">
+        <label> Username </label> <br>
+        <input type="text" name="user_name"> <br>
+        <label> Email </label> <br>
+        <input type="email" name="email"> <br>
+        <label> Password </label> <br>
+        <input type="Password" name="Password"> <br><br>
+        <input type="submit"  value="Submite" name="btn">
+
+    </form>
+    
+
+
+
+
+</body>
+</html>
