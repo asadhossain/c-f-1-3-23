089d5af54bf2bb8fe563fe51aa76d825c2463724
asadhossain <developerasad007@gmail.com>
2023-03-02 01:28:07 +0600

    a

---
+++ from.php
@@ -0,0 +1,13 @@
+<?php
+    if(isset($_POST['btn'])){
+        $userName = $_POST ['user_name'];
+        $email = $_POST ['email'];
+        $password = $_POST ['password'];
+    }
+?>
+    <h3>User Name: <?php if (isset($userName)) {
+                    echo $userName;} ?></h3>
+    <h3>Email: <?php if (isset($email)){
+                    echo $email;   } ?></h3>
+    <h3>Password: <?php if(isset($password)){
+                    echo $password;} ?></h3>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,15 +9,16 @@
 </head>
 <body>
 
-    <form action="" method= "POST">
-        <label> Username </label> <br>
+
+
+    <form action="from" method= "POST">
+        <label>  Username </label> <br>
         <input type="text" name="user_name"> <br>
         <label> Email </label> <br>
         <input type="email" name="email"> <br>
         <label> Password </label> <br>
-        <input type="Password" name="Password"> <br><br>
+        <input type="Password" name="password"> <br><br>
         <input type="submit"  value="Submite" name="btn">
-
     </form>
     
 
